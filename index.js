bfb197add9aa4ed34b25087a243665c6b83b93c0
Juri-Git-Hub <137108726+Juri-Git-Hub@users.noreply.github.com>
2023-10-24 20:31:10 +0200

    public folder; first server structure

--- index.js
+++ index.js
@@ -1,4 +1,23 @@
 import { apiKey } from "./apikey.js";
+import express from "express";
+import axios from "axios";
+import bodyParser from "body-parser";
 
-const weatherApiKey = apiKey
-console.log(weatherApiKey)
+
+const app = express();
+const port = 3000;
+const apikey = apiKey
+
+app.use(express.static("./public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req,res) => {
+    res.render("index.ejs");
+})
+
+
+
+
+app.listen(port, () => {
+    console.log(`This server runs on port ${port}`)
+})
