0f448201c85c453366f1dc6aab2cdbbe5bd18ada
Juri-Git-Hub <137108726+Juri-Git-Hub@users.noreply.github.com>
2023-10-26 20:18:13 +0200

    first css, future days adding

--- index.js
+++ index.js
@@ -8,12 +8,15 @@ const app = express();
 const port = 3000;
 const apikey = apiKey
 
-// app.use(express.static("./public"));
+
+app.use(express.static("./public"));
 app.use(bodyParser.urlencoded({ extended: true }));
+
 
 app.get("/", (req,res) => {
     res.render("index.ejs");
 })
+
 
 app.post("/weather", async (req,res) => {
     const cityData = await axios.get(`http://api.openweathermap.org/geo/1.0/direct?q=${req.body.inputCity}&limit=1&appid=${apikey}`)
@@ -21,8 +24,18 @@ app.post("/weather", async (req,res) => {
     const lon = cityData.data[0].lon
     
     const weatherData = await axios.get(`https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,alerts&units=metric&lang=de&appid=${apikey}`)
-    console.log(weatherData.data.current.weather[0].icon)
-    res.render("index.ejs", { cityName: req.body.inputCity, current: weatherData.data.current })
+    // console.log(weatherData.data.daily[0])
+    res.render("index.ejs", { 
+        cityName: req.body.inputCity, 
+        current: weatherData.data.current, 
+        day1: weatherData.data.daily[0],
+        day2: weatherData.data.daily[1],
+        day3: weatherData.data.daily[2],
+        day4: weatherData.data.daily[3],
+        day5: weatherData.data.daily[4],
+        day6: weatherData.data.daily[5],
+        day7: weatherData.data.daily[6]
+    })
 })
 
 
